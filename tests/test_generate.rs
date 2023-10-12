f738312cf82bfcb74b7570e2cd05a4d2a576dfbf
brosnanyuen <brosnany@uvic.ca>
2023-10-12 00:14:06 -0700

    save

--- tests/test_generate.rs
+++ tests/test_generate.rs
@@ -43,4 +43,39 @@ fn test_hidden_generate() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+    let mut positions_cpu:Vec<f64> = vec![ 0.0,-1.0,6.1,     1.0,-2.0,6.1,          1.0,-1.0,6.1,       7.5, 2.7, 2.1,      1.0,-1.0,6.7,      1.0,-1.0,6.1,         1.0,-1.0,5.1,     1.0,-1.0,5.7];
+    let mut positions = arrayfire::Array::new(&positions_cpu, arrayfire::Dim4::new(&[3, 8, 1, 1]));
+
+    positions = arrayfire::transpose(&positions, false);
+
+
+	//arrayfire::print_gen("positions".to_string(), &positions, Some(6));
+
+	let pivot = vec![0.1, -1.5, 5.5];
+	let idx_intersect2 = RayBNN_Cell::Hidden::Sphere::get_inside_idx_cubeV2(
+		&positions
+		, 1.0
+		, &pivot
+	);
+
+	let mut idx_intersect2_cpu = vec!(u32::default();idx_intersect2.elements());
+	idx_intersect2.host(&mut idx_intersect2_cpu);
+
+	assert_eq!(idx_intersect2_cpu,vec![2, 5, 7]);
+
+
+
+
+
+
 }
