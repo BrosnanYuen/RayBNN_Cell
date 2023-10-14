d65262fccfaffd46435942144d21aa0786ec69bf
brosnanyuen <brosnany@uvic.ca>
2023-10-14 00:12:11 -0700

    save

--- tests/test_collision_minibatch.rs
+++ tests/test_collision_minibatch.rs
@@ -124,6 +124,11 @@ fn test_sphere_cell_collision_minibatch() {
 	}
 
 
+    let mut magsq = arrayfire::pow(&total_obj,&TWO,false);
+    let mut magsq = arrayfire::sum(&magsq,1);
 
+    let (m0,_) = arrayfire::max_all::<f32>(&magsq);
+
+    println!("max {}", m0);
 
 }
