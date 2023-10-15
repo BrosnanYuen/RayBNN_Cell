0678900fdf1c9ce812505fe8aea80424bc9bea99
brosnanyuen <brosnany@uvic.ca>
2023-10-15 16:15:24 -0700

    save

---
+++ tests/test_input.rs
@@ -0,0 +1,22 @@
+#![allow(unused_parens)]
+#![allow(non_snake_case)]
+
+use arrayfire;
+
+
+const BACK_END: arrayfire::Backend = arrayfire::Backend::CUDA;
+const DEVICE: i32 = 0;
+
+
+use rayon::prelude::*;
+
+
+
+#[test]
+fn test_math() {
+    arrayfire::set_backend(BACK_END);
+    arrayfire::set_device(DEVICE);
+
+
+
+}
